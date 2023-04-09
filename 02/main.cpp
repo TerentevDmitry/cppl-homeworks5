983d9befe1fce750d8833e898449a064f0fa2129
TerentevDmitry <Zabey52@gmail.com>
2023-04-09 23:32:14 +0300

    z2+v1

---
+++ 02/main.cpp
@@ -0,0 +1,29 @@
+﻿// Домашнее задание к занятию «Шаблоны и функторы»
+// Задача 2. Таблица
+
+#include <iostream>
+#include "Table.h"
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    Table <double> T1(4, 4);
+    T1.printArr();
+    T1.getSize();
+
+    T1(0, 0) = 3.3;
+    T1(1, 1) = 7.4;
+    T1(2, 2) = 9.5;
+    T1(3, 3) = 10.6;
+
+    T1[3][4] = 99;
+
+    T1.printArr();
+
+    std::cout << T1(1, 1) << "\n";
+
+    std::cout << T1[3][3] << "\n";
+
+    std::cout << "\n\nHello World!\n\n";
+};
